c23cfb4c530ab987582cc70f0aa0a23e0b2c86a0
nobleator <bn2471f9d@gmail.com>
2023-11-03 19:01:59 -0400

    Rebuilding site and publishing on 11/03/2023 19:01:58

--- js/murder/core.js
+++ js/murder/core.js
@@ -1,4 +1,21 @@
+async function checkAnswer() {
+    const message = document.getElementById("answer").value;
+    const msgUint8 = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest("SHA-1", msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    if (hashHex === "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8") {
+        alert("Congratulations, you caught the killer!");
+    } else {
+        alert("Sorry, that's not correct.");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("checkAnswer").addEventListener("click", checkAnswer);
+    
     const decoder = new TextDecoder();
     const url = new Uint8Array([67,108,117,101,32,52,32,111,102,32,55,58,32,84,104,101,32,111,119,110,101,114,32,111,102,32,116,104,101,32,99,97,110,100,108,101,115,116,105,99,107,32,97,114,114,105,118,101,100,32,98,101,102,111,114,101,32,116,104,101,32,111,119,110,101,114,32,111,102,32,116,104,101,32,98,105,99,121,99,108,101,46]);;
     fetch(decoder.decode(url))
